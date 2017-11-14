9af21f5ed166334caed98bdedb7d0ea419394376
David Ball <drball117@gmail.com>
2017-11-13 22:59:58 -0500

    Initial check-in of TicTacToe project.

---
+++ TicTacToeConsole/src/ca/davidrobertball/tictactoeconsole/TicTacToe.java
@@ -0,0 +1,20 @@
+/**
+ * Program Name:	TicTacToe.java
+ * Purpose:				Contains rules for the Tic Tac Toe game and contains
+ * 								the main method to run the game.
+ * Coder:					David Ball, 0692323
+ * Date:					Nov 13, 2017
+ */
+
+package ca.davidrobertball.tictactoeconsole;
+
+public class TicTacToe {
+	
+	
+	
+	public static void main(String[] args) {
+		//Create a TicTacToe object and run the game.
+		
+		
+	}//End of main method.
+}//End of class.
